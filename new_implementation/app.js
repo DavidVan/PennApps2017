7f41a6f7b0df9779599f68593cbf04c81f3fec57
Eunice <echinchilla22@yahoo.com>
2017-09-09 09:51:38 -0700

    updated 'result' to results'

--- new_implementation/app.js
+++ new_implementation/app.js
@@ -4,7 +4,7 @@ var path = require('path');
 
 var index = require('./routes/index.js'); // Include our index.js file from 'routes' folder.
 var search = require('./routes/search.js');
-var result = require('./routes/result.js');
+var results = require('./routes/results.js');
 
 var app = express();
 
@@ -17,7 +17,7 @@ app.set('views', path.join(__dirname, 'views')); // Make sure we have the right 
 app.set('view engine', 'ejs');
 
 app.use('/', index); // Serve the index page.
-app.use('/result', result);
+app.use('/results', results);
 app.use('/api/search', search);
 
 
