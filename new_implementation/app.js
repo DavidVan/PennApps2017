c0908092fb8d211ba089cc4abc521dd57e58c329
Harold Agnote <haroldagnote38@hotmail.com>
2017-09-09 06:55:45 -0700

    Added bootstrap and jquery modules

--- new_implementation/app.js
+++ new_implementation/app.js
@@ -17,6 +17,9 @@ app.set('view engine', 'ejs');
 
 app.use('/', index); // Serve the index page.
 app.use('/api/search', search);
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
 
 app.listen(3000);
 console.log('Server running on port 3000');
