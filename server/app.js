e3bb2723a6b2858104d6c2293521c84341712049
David Van <David@DavidVan.net>
2017-09-09 00:48:42 -0700

    Fixed.

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ var bodyParser = require('body-parser');
 var express = require('express');
 var path = require('path');
 
-var index = require('./routes/index'); // Include our index.js file from 'routes' folder.
+var helloworld = require('./routes/helloworld.js'); // Include our index.js file from 'routes' folder.
 var search = require('./routes/search.js');
 
 var app = express();
@@ -13,8 +13,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public'))); // Expose the 'public' folder.
 app.set('views', path.join(__dirname, 'views')); // Make sure we have the right 'views' folder defined.
 
-app.use('/', index); // Serve the index page.
-app.use('/search', search);
+app.use('/api/helloworld', helloworld); // Serve the index page.
+app.use('/api/search', search);
 
 app.listen(3001);
 console.log('Server running on port 3001');
