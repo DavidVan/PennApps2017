961d1ffa7c9b2d0090a14ca195c99a476fc189ff
Harold Agnote <haroldagnote38@hotmail.com>
2017-09-09 08:57:05 -0700

    Fixed search/result stuff

--- new_implementation/app.js
+++ new_implementation/app.js
@@ -4,6 +4,7 @@ var path = require('path');
 
 var index = require('./routes/index.js'); // Include our index.js file from 'routes' folder.
 var search = require('./routes/search.js');
+var result = require('./routes/result.js');
 
 var app = express();
 
@@ -16,7 +17,7 @@ app.set('views', path.join(__dirname, 'views')); // Make sure we have the right 
 app.set('view engine', 'ejs');
 
 app.use('/', index); // Serve the index page.
-app.use('/result', search);
+app.use('/result', result);
 app.use('/api/search', search);
 
 
--- new_implementation/routes/result.js
+++ new_implementation/routes/result.js
@@ -1,3 +1,13 @@
 var bodyParser = require('body-parser');
 var express = require('express');
 var path = require('path');
+var router = express.Router();
+
+router.post('/', function(req,res,next) {
+    var search = req.body.searchTerm;
+    console.log('Got a POST request');
+    console.log(search);
+    res.send(search);
+});
+
+module.exports = router;
