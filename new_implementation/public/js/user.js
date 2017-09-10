e2af9c54c2515dcd43ab532230434814938e3ec1
Harold Agnote <haroldagnote38@hotmail.com>
2017-09-09 20:21:16 -0700

    Finished login implementation

--- new_implementation/public/js/user.js
+++ new_implementation/public/js/user.js
@@ -3,7 +3,7 @@ var user = "";
 if (localStorage.getItem("user") == null)
 {
     user = "Guest";
-    localStorage.setItem("user","Guest" );
+    localStorage["user"]="Guest";
     console.log("Logged in as Guest");
 }
 else
@@ -14,12 +14,33 @@ else
 function login(username)
 {
     user = username;
+    localStorage["user"] = username;
     console.log("Logged in as " + username);
 }
 
-function  logout()
+function logout()
 {
     user = "Guest";
+    localStorage.clear();
     console.log("Logged out as " + username);
 }
 
+function changeName(idName)
+{
+    var name = document.getElementById(idName);
+    name.innerHTML = user;
+}
+
+
+function assignLogin()
+{
+    var name = document.getElementById('accountButton');
+    if (user !== "Guest")
+    {
+        name.href = '/account'
+    }
+    else
+    {
+        name.href = '/login' 
+    }
+}
